7c2b7d48e0867373830aa4524a80f210f18a3152
divyendu dutta <connect2divyendu@gmail.com>
2019-12-14 21:54:22 +0530

    war file changes

--- src/main/java/com/divyendu/pluralsightbasics/conference/PluralsightbasicsApplication.java
+++ src/main/java/com/divyendu/pluralsightbasics/conference/PluralsightbasicsApplication.java
@@ -2,9 +2,10 @@ package com.divyendu.pluralsightbasics.conference;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PluralsightbasicsApplication {
+public class PluralsightbasicsApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(PluralsightbasicsApplication.class, args);
